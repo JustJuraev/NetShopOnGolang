69118db4b70547ab4dfbb7f6132fb05734a4836d
Abdusamad Juraev <juraevabdusamad517@gmail.com>
2023-08-23 16:20:27 +0500

    added category

--- main.go
+++ main.go
@@ -20,8 +20,49 @@ type Product struct {
 	CategoryId int
 	Image      string
 }
+type Category struct {
+	Id    int
+	Name  string
+	Image string
+}
 
 var produts = []Product{}
+var categories = []Category{}
+var productsbycat = []Product{}
+
+func GetByCategoty(page http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	connStr := "user=postgres password=123456 dbname=netshopgolang sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	row, err := db.Query("SELECT * FROM public.products WHERE categoryid = $1", id)
+
+	productsbycat = []Product{}
+	for row.Next() {
+		var prd Product
+		err = row.Scan(&prd.Id, &prd.Name, &prd.Price, &prd.ShortDesc, &prd.LongDesc, &prd.CategoryId, &prd.Image)
+		if err != nil {
+			panic(err)
+		}
+
+		productsbycat = append(productsbycat, prd)
+
+	}
+
+	tmpl, err := template.ParseFiles("html_files/category.html", "html_files/header.html")
+	if err != nil {
+		panic(err)
+	}
+	tmpl.ExecuteTemplate(page, "category", productsbycat)
+}
 
 func GetProduct(page http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -75,12 +116,39 @@ func index(page http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			panic(err)
 		}
+
 		produts = append(produts, prd)
 
 	}
 
+	defer res.Close()
+
+	res2, err := db.Query("SELECT * FROM public.categories")
+
+	if err != nil {
+		panic(err)
+	}
+
+	categories = []Category{}
+	for res2.Next() {
+		var cat Category
+		err = res2.Scan(&cat.Id, &cat.Name, &cat.Image)
+		if err != nil {
+			panic(err)
+		}
+		categories = append(categories, cat)
+	}
+
+	data := struct {
+		Array1 []Category
+		Array2 []Product
+	}{
+		Array2: produts,
+		Array1: categories,
+	}
+
 	tmpl, err := template.ParseFiles("html_files/index.html", "html_files/header.html")
-	tmpl.ExecuteTemplate(page, "index", produts)
+	tmpl.ExecuteTemplate(page, "index", data)
 }
 
 func main() {
@@ -97,7 +165,9 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", index)
+
 	router.HandleFunc("/product/{id:[0-9]+}", GetProduct).Methods("GET")
+	router.HandleFunc("/category/{id:[0-9]+}", GetByCategoty).Methods("GET")
 
 	http.Handle("/", router)
 	http.ListenAndServe(":8080", nil)
