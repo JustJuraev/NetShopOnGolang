fc28df6f3e69c388353c365584e2def714ebe936
Abdusamad Juraev <juraevabdusamad517@gmail.com>
2023-08-24 20:51:14 +0500

    added basket

--- main.go
+++ main.go
@@ -2,23 +2,26 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 
+	//"encoding/gob"
 	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	_ "github.com/lib/pq"
 )
 
 type Product struct {
-	Id         int
-	Name       string
-	Price      int
-	ShortDesc  string
-	LongDesc   string
-	CategoryId int
-	Image      string
+	Id         int    `json:"id"`
+	Name       string `json:"name"`
+	Price      int    `json:"price"`
+	ShortDesc  string `json:"shortdesc"`
+	LongDesc   string `json:"longdesc"`
+	CategoryId int    `json:"categoryid"`
+	Image      string `json:"image"`
 }
 type Category struct {
 	Id    int
@@ -26,9 +29,140 @@ type Category struct {
 	Image string
 }
 
+type BasketProduct struct {
+	Id         int    `json:"id"`
+	Name       string `json:"name"`
+	Price      int    `json:"price"`
+	CategoryId int    `json:"categoryid"`
+	Image      string `json:"image"`
+	Count      int    `json:"count"`
+}
+
 var produts = []Product{}
 var categories = []Category{}
 var productsbycat = []Product{}
+var basket = []BasketProduct{}
+
+var str string
+
+var store = sessions.NewCookieStore([]byte("basket-secret"))
+
+func contains(product []BasketProduct, prd Product) bool {
+	for _, v := range product {
+		if v.Id == prd.Id {
+			return true
+		}
+	}
+
+	return false
+}
+
+func GetIndex(product []BasketProduct, prd Product) int {
+
+	var index int
+
+	for idx, v := range product {
+		if v.Id == prd.Id {
+			index = idx
+		}
+	}
+
+	return index
+}
+
+func GetProductFromBasket(product []BasketProduct, prd Product) BasketProduct {
+
+	var pr BasketProduct
+
+	for _, v := range product {
+		if v.Id == prd.Id {
+			pr = v
+		}
+	}
+
+	return pr
+}
+
+func AddToBasket(page http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	connStr := "user=postgres password=123456 dbname=netshopgolang sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	//fmt.Println(id)
+
+	row := db.QueryRow("SELECT * FROM public.products WHERE id = $1", id)
+	//fmt.Println(id)
+	prd := Product{}
+	err = row.Scan(&prd.Id, &prd.Name, &prd.Price, &prd.ShortDesc, &prd.LongDesc, &prd.CategoryId, &prd.Image)
+	if err != nil {
+		http.Error(page, http.StatusText(404), http.StatusNotFound)
+		fmt.Println(err)
+	} else {
+		if contains(basket, prd) == false {
+			basketprd := BasketProduct{
+				Id:         prd.Id,
+				Name:       prd.Name,
+				Price:      prd.Price,
+				CategoryId: prd.CategoryId,
+				Image:      prd.Image,
+				Count:      1,
+			}
+			basket = append(basket, basketprd)
+			//fmt.Println(basket)
+		} else {
+			var s = GetProductFromBasket(basket, prd)
+			s.Count++
+			var idx = GetIndex(basket, prd)
+			basket[idx] = basket[len(basket)-1]
+			basket[len(basket)-1] = BasketProduct{}
+			basket = basket[:len(basket)-1]
+			basket = append(basket, s)
+		}
+
+	}
+	jsonBytes, err := json.Marshal(&basket)
+	session, _ := store.Get(r, "sesssion")
+	session.Values["basket"] = string(jsonBytes)
+	session.Save(r, page)
+
+	http.Redirect(page, r, "/basket", http.StatusSeeOther)
+
+}
+
+func ShowBasket(page http.ResponseWriter, r *http.Request) {
+
+	session, _ := store.Get(r, "sesssion")
+	untyped, ok := session.Values["basket"]
+
+	if !ok {
+		panic(ok)
+	}
+
+	basket, ok := untyped.(string)
+	//	fmt.Println(basket)
+
+	basketP := []BasketProduct{}
+	err := json.Unmarshal([]byte(basket), &basketP)
+
+	if err != nil {
+		panic(err)
+	}
+
+	tmpl, err := template.ParseFiles("html_files/basket.html", "html_files/header.html")
+	if err != nil {
+		panic(err)
+	}
+	tmpl.ExecuteTemplate(page, "basket", basketP)
+}
 
 func GetByCategoty(page http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -166,8 +300,10 @@ func main() {
 
 	router.HandleFunc("/", index)
 
+	router.HandleFunc("/basket", ShowBasket)
 	router.HandleFunc("/product/{id:[0-9]+}", GetProduct).Methods("GET")
 	router.HandleFunc("/category/{id:[0-9]+}", GetByCategoty).Methods("GET")
+	router.HandleFunc("/AddToBasket/{id:[0-9]+}", AddToBasket).Methods("POST")
 
 	http.Handle("/", router)
 	http.ListenAndServe(":8080", nil)
